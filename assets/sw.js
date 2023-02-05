b7011cad616a675aa71d1865b4e94f1c1152aef7
Ty Briscoe <tybriscoe@tangital.com>
2023-02-05 06:09:45 +0000

    initial commit

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './one-page-template.js',
+  './one-page-template_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
